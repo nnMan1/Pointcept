88fdd958d9fd421309b2b5b782078f8f83921108
Velibor Dosljak <dosljakvelibor@gmail.com>
2024-04-03 10:20:10 +0200

    Updated .gitignore

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -4,16 +4,7 @@ docker run -d -ti \
 		  -v /tmp/.X11-unix:/tmp\.X11-unix --net=host \
 		  --shm-size 8G \
           	  -v $(pwd):/home \
-          	  -v /media/velibor/Data/PhD_data/PhD/PartNet/:/home/data/Partnet\
-		  -v /home/velibor/Projects/data/ModelNet40/:/home/data/ModelNet40 \
-          	  -v /media/velibor/Data/PhD_data/STL_Data/classes/:/home/data/LEAP_synth/raw/classes \
-          	  -v /media/velibor/Data/PhD_data/STL_Data/artefacts/:/home/data/LEAP_synth/raw/artefacts \
-          	  -v /media/velibor/Data/PhD_data/STL_Data/contexts/:/home/data/LEAP_synth/raw/contexts \
-          	  -v /media/velibor/Data/PhD_data/theoreticalPoses/:/home/data/LEAP_synth/raw/theoretical_poses \
-          	  -v '/media/velibor/Data/PhD_data/ABC dataset/chunks':'/home/data/ABCDataset/raw' \
-          	  -v '/media/velibor/Data/PhD_data/AssemblyRepository/processed/':'/home/data/ABCDataset/processed' \
-          	  -v /media/velibor/Data/PhD_data/metadata/:/home/data/LEAP/raw \
-  		  -v /media/velibor/Data/PhD_data/AssemblyRepository/scans/:/home/data/assembly/raw \
-    		  -v /media/velibor/Data/PhD_data/AssemblyRepository/processed/:/home/data/assembly/scanns \
+			  -v /home/velibor/Data/scannet:/home/data/scannet \
+			  -v /home/velibor/Data/AssemblyRepository/scans/:/home/data/assembly/raw \
 		  --gpus all -it --rm --name "pointcept" pointcept/pointcept:pytorch2.0.1-cuda11.7-cudnn8-devel
 		  
