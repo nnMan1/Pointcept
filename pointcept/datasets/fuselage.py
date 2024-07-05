14f08f3e799dfbc2c8c7e63c19b972492cc85dbe
Velibor Dosljak <dosljakvelibor@gmail.com>
2024-07-05 12:33:05 +0200

    Added fuselage dataset and config

--- pointcept/datasets/fuselage.py
+++ pointcept/datasets/fuselage.py
@@ -1,14 +1,8 @@
 import os
 import glob
-<<<<<<< HEAD
 import open3d as o3d
 import numpy as np
 import torch
-=======
-import h5py
-import numpy as np
-import json
->>>>>>> db8ad9c1e69143cd82a06689660abe8bfbd62b2e
 from copy import deepcopy
 from torch.utils.data import Dataset
 from collections.abc import Sequence
@@ -19,12 +13,7 @@ from pointcept.utils.cache import shared_dict
 
 from .transform import Compose, TRANSFORMS
 from .builder import DATASETS
-<<<<<<< HEAD
 from .transform import Compose, TRANSFORMS
-=======
-
-import open3d as o3d
->>>>>>> db8ad9c1e69143cd82a06689660abe8bfbd62b2e
 
 @DATASETS.register_module()
 class Fuselage(Dataset):
@@ -32,18 +21,13 @@ class Fuselage(Dataset):
     def __init__(
         self,
         split="train",
-<<<<<<< HEAD
         data_root="data/fuselage/crops",
-=======
-        data_root="data/fuselage",
->>>>>>> db8ad9c1e69143cd82a06689660abe8bfbd62b2e
         transform=None,
         ignore_index=-1,
         test_mode=False,
         test_cfg=None,
         cache=False,
         loop=1,
-<<<<<<< HEAD
         classes = ['body', 'body1', 'hole', 'panel', 'riwet', 'table']
     ):
         super(Fuselage, self).__init__()
@@ -51,10 +35,6 @@ class Fuselage(Dataset):
         self.classes = classes
         self.class_to_id = {cls: i for (i, cls) in enumerate(classes)}
 
-=======
-    ):
-        super(Fuselage, self).__init__()
->>>>>>> db8ad9c1e69143cd82a06689660abe8bfbd62b2e
         self.data_root = data_root
         self.split = split
         self.transform = Compose(transform)
@@ -84,7 +64,6 @@ class Fuselage(Dataset):
             )
         )
 
-<<<<<<< HEAD
     def get_data_list(self):
         
         if isinstance(self.split, str):
@@ -99,27 +78,12 @@ class Fuselage(Dataset):
         data_list = [f.strip() for f in data_list]
 
         return data_list
-=======
-        with open(os.path.join(self.data_root, 'raw', 'labels.json')) as f:
-            self.semantic_mapping = np.asarray(json.load(f))
-
-    def get_data_list(self):
-        dl = glob.glob(os.path.join(self.data_root, 'raw', '*', '*.npy'))
-
-        if self.split == 'train':
-            dl = dl[:int(0.8*len(dl))]
-        else:
-            dl =dl[int(0.8*len(dl)):]
-
-        return dl
->>>>>>> db8ad9c1e69143cd82a06689660abe8bfbd62b2e
 
     def get_data(self, idx):
 
         idx = idx % len(self.data_list)
 
         data = self.data_list[idx]
-<<<<<<< HEAD
         # print(os.path.join(self.data_root, 'scanns', data))
 
         labels = []
@@ -139,25 +103,12 @@ class Fuselage(Dataset):
         return {
             'coord': np.asarray(pcd.points),
             'segment': labels.astype(np.int32),
-=======
-        # data = o3d.io.read_triangle_mesh(data).sample_points_uniformly(50000)
-        data = np.load(data)
-        
-        return {
-            'coord': data[::5, :3],
-            'instance': data[::5, 3].astype(np.int64),
-            'segment': self.semantic_mapping[data[::5, 3].astype(np.int64)],
->>>>>>> db8ad9c1e69143cd82a06689660abe8bfbd62b2e
             'id': idx,
             'path': self.data_list[idx]
         } 
 
     def get_data_name(self, idx):
-<<<<<<< HEAD
         return str(self.data_list[idx]).replace('/', '_')
-=======
-        return str(idx)
->>>>>>> db8ad9c1e69143cd82a06689660abe8bfbd62b2e
 
     def prepare_train_data(self, idx):
         # load data
@@ -198,9 +149,5 @@ class Fuselage(Dataset):
             return self.prepare_train_data(idx)
 
     def __len__(self):
-<<<<<<< HEAD
         return len(self.data_list) * self.loop
 
-=======
-        return len(self.data_list) * self.loop
->>>>>>> db8ad9c1e69143cd82a06689660abe8bfbd62b2e
