4ffcb1d3fed6575d141bcd72ad916363054f237d
Velibor Dosljak <vdosljak@login07.leonardo.local>
2025-09-08 22:05:00 +0200

    Fix back propagation through dinoV2

--- pointcept/models/feature_extractors/dino_v2_feature_extractor.py
+++ pointcept/models/feature_extractors/dino_v2_feature_extractor.py
@@ -33,61 +33,68 @@ class DinoV2FeatureExtractor(BaseFeatureExtractor):
         self.model = Dinov2Model.from_pretrained(model_name, 
                                                  local_files_only=True)
         
-        self.proj = nn.Linear(self.fts_dim, self.out_fts_dim)
+        self.proj = nn.Sequential(
+                nn.Linear(fts_dim, out_fts_dim),
+                nn.LayerNorm(out_fts_dim),
+                nn.ReLU(),
+                nn.Linear(out_fts_dim, out_fts_dim)
+            )
 
     def backbone_modules(self):
         return [self.model]
 
     def forward_features(self, batch: Batch) -> Dict[str, Tensor]:
 
-        images = batch.get('images', [])
+        with torch.no_grad():
+            images = batch.get('images', [])
 
-        mesh_features = torch.zeros((len(batch['coord']), self.fts_dim), dtype=torch.float32, device=self.model.device)
-        mesh_features_cnt = torch.zeros((len(batch['coord'])), dtype=torch.float32, device=self.model.device)
+            mesh_features = torch.zeros((len(batch['coord']), self.fts_dim), dtype=torch.float32, device=self.model.device)
+            mesh_features_cnt = torch.zeros((len(batch['coord'])), dtype=torch.float32, device=self.model.device)
 
-        bs, ibs, mbs, obs = 0, 0, 0, 0
-        i=0
+            bs, ibs, mbs, obs = 0, 0, 0, 0
+            i=0
 
-        for be, ibe, mbe, obe in zip(batch['offset'], batch['image_offset'], batch['mappings_offset'], batch['origin_offset']):
-            outputs = self.model(images[ibs: ibe], output_hidden_states=True)
+            for be, ibe, mbe, obe in zip(batch['offset'], batch['image_offset'], batch['mappings_offset'], batch['origin_offset']):
+                outputs = self.model(images[ibs: ibe], output_hidden_states=True)
 
-            patch_tokens = outputs.last_hidden_state[:, 1:, :]  # remove CLS token
+                patch_tokens = outputs.last_hidden_state[:, 1:, :]  # remove CLS token
 
-            n_patches = patch_tokens.shape[1]
-            dim = int(n_patches ** 0.5)
-            assert dim * dim == n_patches, "Patch tokens are not square!"
+                n_patches = patch_tokens.shape[1]
+                dim = int(n_patches ** 0.5)
+                assert dim * dim == n_patches, "Patch tokens are not square!"
 
-            img_size = torch.tensor(images[ibs].shape[-2:], dtype=torch.int32)
-            div_factor = 512 // dim
+                img_size = torch.tensor(images[ibs].shape[-2:], dtype=torch.int32)
+                div_factor = 512 // dim
 
-            patch_tokens = patch_tokens.reshape(patch_tokens.shape[0], dim, dim, -1) 
+                patch_tokens = patch_tokens.reshape(patch_tokens.shape[0], dim, dim, -1) 
 
-            features = patch_tokens.to(self.model.device)
+                features = patch_tokens.to(self.model.device)
 
-            mappings_src = batch['mappings_src'][mbs:mbe]
-            mappings_tgt = batch['mappings_tgt'][mbs:mbe]
-            inverse = batch['inverse'][obs:obe]
-            mappings_tgt = inverse[mappings_tgt] # Apply inverse mapping from points to voxels
+                mappings_src = batch['mappings_src'][mbs:mbe]
+                mappings_tgt = batch['mappings_tgt'][mbs:mbe]
+                inverse = batch['inverse'][obs:obe]
+                mappings_tgt = inverse[mappings_tgt] # Apply inverse mapping from points to voxels
 
-            if self.merge_strategy == 'random_sample':
-                random_positions = self.one_random_position_per_value(mappings_tgt)
+                if self.merge_strategy == 'random_sample':
+                    random_positions = self.one_random_position_per_value(mappings_tgt)
 
-                mappings_src = mappings_src[random_positions]
-                mappings_tgt = mappings_tgt[random_positions]
+                    mappings_src = mappings_src[random_positions]
+                    mappings_tgt = mappings_tgt[random_positions]
 
-            a, b, c = mappings_src.T
-            b //= div_factor
-            c //= div_factor
+                a, b, c = mappings_src.T
+                b //= div_factor
+                c //= div_factor
 
-            mesh_features[bs:be][mappings_tgt] += features[a, b, c]
-            mesh_features_cnt[bs:be][mappings_tgt] += 1
+                mesh_features[bs:be][mappings_tgt] += features[a, b, c]
+                mesh_features_cnt[bs:be][mappings_tgt] += 1
 
-            bs = be
-            ibs = ibe
-            mbs = mbe
-            obs = obe
+                bs = be
+                ibs = ibe
+                mbs = mbe
+                obs = obe
 
-        mesh_features[mesh_features_cnt > 0] = mesh_features[mesh_features_cnt > 0] / mesh_features_cnt[mesh_features_cnt > 0][..., None]
+            mesh_features[mesh_features_cnt > 0] = mesh_features[mesh_features_cnt > 0] / mesh_features_cnt[mesh_features_cnt > 0][..., None]
+            
         mesh_features = self.proj(mesh_features)
 
         return {
