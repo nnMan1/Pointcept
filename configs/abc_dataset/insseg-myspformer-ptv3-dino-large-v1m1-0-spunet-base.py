0b3ed0b5e9e4ebb8780c2545b3065b2a9ba88842
Velibor Dosljak <dosljakvelibor@gmail.com>
2026-02-14 18:48:48 +0100

    Added config

--- configs/abc_dataset/insseg-myspformer-ptv3-dino-large-v1m1-0-spunet-base.py
+++ configs/abc_dataset/insseg-myspformer-ptv3-dino-large-v1m1-0-spunet-base.py
@@ -19,110 +19,122 @@ segment_ignore_index = (-1, )
 
 # model settings
 model = dict(
-    type="MySPFormer",
-    num_query = 100,
+    type='MySPFormer',
+    num_query=100,
     encoder=dict(
         backbone=dict(
-            in_channels=3,
-            order=["z", "z-trans", "hilbert", "hilbert-trans"],
-            stride=(2, 2, 2, 2),
-            enc_depths=(2, 2, 2, 6, 2),
-            enc_channels=(32, 64, 128, 256, 512),
-            enc_num_head=(2, 4, 8, 16, 32),
-            enc_patch_size=(1024, 1024, 1024, 1024, 1024),
-            dec_depths=(2, 2, 2, 2),
-            dec_channels=(64, 64, 128, 256),
-            dec_num_head=(4, 4, 8, 16),
-            dec_patch_size=(1024, 1024, 1024, 1024),
-            mlp_ratio=4,
-            qkv_bias=True,
-            qk_scale=None,
-            attn_drop=0.0,
-            proj_drop=0.0,
-            drop_path=0.3,
-            shuffle_orders=True,
-            pre_norm=True,
-            enable_rpe=False,
-            enable_flash=True,
-            upcast_attention=False,
-            upcast_softmax=False,
-            cls_mode=False,
-            pdnorm_bn=False,
-            pdnorm_ln=False,
-            pdnorm_decouple=True,
-            pdnorm_adaptive=False,
-            pdnorm_affine=True,
-            pdnorm_conditions=("ScanNet", "S3DIS", "Structured3D"),
-        ),
-        out_channels=32,
-        dino_version="facebook/dinov2-large",  
-        dino_output_size=1024
-     ),
-     decoder=dict(
+            type='MergeFeatures',
+            model1_config=dict(
+                type='Image2PointCLoud',
+                model_type = "DinoV2",
+                model_name = "facebook/dinov2-large",
+                fts_dim=384,
+                merge_strategy='mean',
+                return_features=['feat'],
+                freeze_backbone=True,
+                freeze_backbone_bn=True),
+            model2_config=dict(
+                type='PT-V3FeatureExtractor',
+                in_channels=3,
+                order=['z', 'z-trans', 'hilbert', 'hilbert-trans'],
+                stride=(2, 2, 2, 2),
+                enc_depths=(2, 2, 2, 6, 2),
+                enc_channels=(32, 64, 128, 256, 512),
+                enc_num_head=(2, 4, 8, 16, 32),
+                enc_patch_size=(1024, 1024, 1024, 1024, 1024),
+                dec_depths=(2, 2, 2, 2),
+                dec_channels=(64, 64, 128, 256),
+                dec_num_head=(4, 4, 8, 16),
+                dec_patch_size=(1024, 1024, 1024, 1024),
+                mlp_ratio=4,
+                qkv_bias=True,
+                qk_scale=None,
+                attn_drop=0.0,
+                proj_drop=0.0,
+                drop_path=0.3,
+                shuffle_orders=True,
+                pre_norm=True,
+                enable_rpe=False,
+                enable_flash=True,
+                upcast_attention=False,
+                upcast_softmax=False,
+                cls_mode=False,
+                pdnorm_bn=False,
+                pdnorm_ln=False,
+                pdnorm_decouple=True,
+                pdnorm_adaptive=False,
+                pdnorm_affine=True,
+                pdnorm_conditions=('ScanNet', 'S3DIS', 'Structured3D'),
+                return_features=['feat'])),
+        backbone_out_channels=64,
+        out_channels=32),
+    decoder=dict(
         in_channels=32,
         hlevels=6,
         mask_modules=[
             dict(
-                num_classes=num_classes, 
-                return_attn_masks=True, 
-                hidden_dim=fts_sizes,
-                reuse=1
-            )
+                num_classes=1, return_attn_masks=True, hidden_dim=128, reuse=1)
         ],
         query_refinement_modules=[
             dict(
                 in_channels=128,
-                mask_dim=fts_sizes,
-                dim_feedforward=dim_feedforward,
+                mask_dim=128,
+                dim_feedforward=1024,
                 pre_norm=False,
-                num_heads=8, 
-                dropout=0
-            ),
+                num_heads=8,
+                dropout=0),
             dict(
                 in_channels=128,
-                mask_dim=fts_sizes,
-                dim_feedforward=dim_feedforward,
+                mask_dim=128,
+                dim_feedforward=1024,
                 pre_norm=False,
-                num_heads=8, 
-                dropout=0
-            ),
+                num_heads=8,
+                dropout=0),
             dict(
                 in_channels=128,
-                mask_dim=fts_sizes,
-                dim_feedforward=dim_feedforward,
+                mask_dim=128,
+                dim_feedforward=1024,
                 pre_norm=False,
-                num_heads=8, 
-                dropout=0
-            ),
+                num_heads=8,
+                dropout=0),
             dict(
                 in_channels=128,
-                mask_dim=fts_sizes,
-                dim_feedforward=dim_feedforward,
+                mask_dim=128,
+                dim_feedforward=1024,
                 pre_norm=False,
-                num_heads=8, 
-                dropout=0
-            ),
+                num_heads=8,
+                dropout=0),
             dict(
                 in_channels=128,
-                mask_dim=fts_sizes,
-                dim_feedforward=dim_feedforward,
+                mask_dim=128,
+                dim_feedforward=1024,
                 pre_norm=False,
-                num_heads=8, 
-                dropout=0
-            ),
+                num_heads=8,
+                dropout=0),
             dict(
                 in_channels=128,
-                mask_dim=fts_sizes,
-                dim_feedforward=dim_feedforward,
+                mask_dim=128,
+                dim_feedforward=1024,
                 pre_norm=False,
-                num_heads=8, 
-                dropout=0
-            )
+                num_heads=8,
+                dropout=0)
+        ]),
+    matcher=dict(
+        type='HungarianMatcher',
+        cost_terms=[
+            dict(type='ClassCost', weight=0.5, enabled=True, use_logits=False),
+            dict(
+                type='MaskBCECost',
+                weight=1.0,
+                enabled=True,
+                instance_ignore_index=-1),
+            dict(
+                type='MaskDiceCost',
+                weight=1.0,
+                enabled=True,
+                instance_ignore_index=-1)
         ],
-    ),
-    instance_ignore_index=-1,
-)
-
+        instance_ignore_index=-1))
 
 # scheduler settings
 epoch = 500
@@ -137,8 +149,8 @@ scheduler = dict(
 )
 
 # dataset settings
-dataset_type = "MechanicalAssemblySynth"
-data_root = "data/segment-assembly-synthetic/data"
+dataset_type = "HDF5_Dataset"
+data_root = "data/segment-assembly-synthetic/data/abc_dataset/processed"
 recompute_clustering=False
 
 classes={"other": 0, 
@@ -157,9 +169,8 @@ data = dict(
     train=dict(
         type=dataset_type,
         split="train",
-        cache=True,
         data_root=data_root,
-        recompute_clustering=recompute_clustering,
+        load_images=True,
         transform=[
             dict(type="CenterShift", apply_z=True),
             # dict(
@@ -190,7 +201,7 @@ data = dict(
                 mode="train",
                 return_inverse=True,
                 return_grid_coord=True,
-                keys=("coord", "normal", "segment", "instance", 'seg_indices'),
+                keys=("coord", "normal", "segment", "instance"),
             ),
             # dict(type="SphereCrop",  point_max=200000, mode="random"),
             dict(
@@ -210,15 +221,19 @@ data = dict(
                     "images",
                     "mappings_src",
                     "mappings_tgt",
-                    # "instance_centroid",
-                    # "bbox",
-                    "seg_indices",
+                    "instance_segment",
                     "path",
                     "name",
                     "inverse"
                 ),
                 feat_keys=("coord"),
-                offset_keys_dict=dict(offset="coord", origin_offset="origin_coord", image_offset="images", mappings_offset="mappings_src"),
+                offset_keys_dict=dict(
+                    offset="coord", 
+                    origin_offset="origin_coord", 
+                    image_offset="images", 
+                    mappings_offset="mappings_src",
+                    instance_segment_offset="instance_segment"
+                ),
             ),
         ],
         test_mode=False,
@@ -228,8 +243,7 @@ data = dict(
         type=dataset_type,
         split="val",
         data_root=data_root,
-        cache=True,
-        recompute_clustering=recompute_clustering,
+        load_images=True,
         transform=[
             dict(type="CenterShift", apply_z=True),
             dict(
@@ -248,7 +262,7 @@ data = dict(
                 mode="train",
                 return_inverse=True,
                 return_grid_coord=True,
-                keys=("coord", "segment", "instance", "seg_indices"),
+                keys=("coord", "segment", "instance"),
             ),
             # dict(type="SphereCrop", point_max=1000000, mode='center'),
             dict(type="CenterShift", apply_z=False),
@@ -270,15 +284,19 @@ data = dict(
                     "mappings_src",
                     "mappings_tgt",
                     'origin_coord', 'origin_segment', 'origin_instance',
-                    # "instance_centroid",
-                    # "bbox",
-                    "seg_indices",
+                    "instance_segment",
                     "path",
                     "name",
                     "inverse"
                 ),
                 feat_keys=('coord'),
-                offset_keys_dict=dict(offset="coord", origin_offset="origin_coord", image_offset="images", mappings_offset="mappings_src"),
+                offset_keys_dict=dict(
+                    offset="coord", 
+                    origin_offset="origin_coord", 
+                    image_offset="images", 
+                    mappings_offset="mappings_src", 
+                    instance_segment_offset="instance_segment"
+                ),
             ),
         ],
         test_mode=False,
