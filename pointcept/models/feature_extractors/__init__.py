0595e4d13b81fe0527fa84c055a0fb45a876112b
Velibor Dosljak <dosljakvelibor@gmail.com>
2026-03-02 10:59:14 +0100

    Remove dinov2.v3 from init

--- pointcept/models/feature_extractors/__init__.py
+++ pointcept/models/feature_extractors/__init__.py
@@ -1,5 +1,3 @@
-from .dino_v2_feature_extractor import *
-from .dino_v3_feature_extractor import *
 from .merge_features import *
 from .point_transformer_v3_feature_extractor import *
 from .image_feature_extractor import *
